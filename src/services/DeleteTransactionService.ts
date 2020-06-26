e3c0e043af57766fb0aa78b56fd5a1a1e2ae8d18
Adilson Vicente dos Santos <adilson_mumu@hotmail.com>
2020-06-26 16:10:01 -0300

    :heavy_check_mark: Challenge finalized

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,20 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+
+    const transactions = await transactionRepository.findOne(id);
+
+    if (!transactions) {
+      throw new AppError('Transaction does not exist');
+    }
+
+    await transactionRepository.remove(transactions);
   }
 }
 
